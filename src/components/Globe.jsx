bb2806cc00bb49c4a4ca174bb638fe9131e03718
Mohd Amaan <mhdamaan79@gmail.com>
2025-03-07 19:47:03 +0530

    fix globe height

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -416,7 +416,7 @@ export function Globe() {
           </p>
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-        <div className="absolute w-full -bottom-20 h-72 md:h-auto z-10">
+        <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
           <Suspense
             fallback={<div className="text-center">Loading globe...</div>}
           >
